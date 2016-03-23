fa2154a45ebd93097ec8ac28fb079995e5c2b170
amansan <amansan@gmail.com>
2016-03-22 23:08:37 -0300

    Andrei
    Regulamento

--- www/js/controllers/appController.js
+++ www/js/controllers/appController.js
@@ -2,18 +2,11 @@ angular.module('starter.controllers')
 
 .controller('AppCtrl',AppCtrl);
 
-AppCtrl.$inject = ['$scope','$location','$window','$ionicViewService','auth','store'];
+AppCtrl.$inject = ['$scope','$location','$window','$ionicViewService'];
 
-function AppCtrl($scope,$location,$window,$ionicViewService,auth,store){
+function AppCtrl($scope,$location,$window,$ionicViewService){
 
-	$scope.auth = auth;
-
-	$scope.logOut = function(){
-		auth.signout();
-		store.remove('profile');
-		store.remove('token');
-		$location.path('/login');
-	}
+	
 
 
 }
--- www/lib/auth0.js/version.js
+++
@@ -1,1 +0,0 @@
-module.exports = { str: "6.8.0" };
